989c45a2d45ad25663ae3287c43d1d91de84dc25
KittenSmasher <liaunardicindy@gmail.com>
2023-12-07 21:07:03 +0700

    conf not working

--- project.py
+++ project.py
@@ -12,6 +12,17 @@ def clear():
     else:
         os.system('clear')
 
+# DATASET
+dataset_path = './Dataset'
+dataset_dir = os.listdir(dataset_path)
+
+# PRE-TRAINED MODEL
+models = cv.CascadeClassifier('./pre-trained-models/haarcascade_frontalface_default.xml')
+
+# FACE RECOGNIZER
+recognizer = cv.face.LBPHFaceRecognizer_create()
+model_trained = False
+
 # menu
 def main():
     clear()
@@ -38,11 +49,9 @@ def main():
             break
 
 def train_and_test():
-    # pre-trained models
-    models = cv.CascadeClassifier('./pre-trained-models/haarcascade_frontalface_default.xml')
-
-    # datasets
-    dataset_path = './Dataset'
+    global model_trained
+    global models
+    global recognizer
     
   # split dataset
     train_list = []
@@ -55,10 +64,10 @@ def train_and_test():
     # convert label to integer
     label_map = {}
 
-    for i, folder in enumerate(os.listdir(dataset_path)):
+    for i, folder in enumerate(dataset_dir):
         label_map[folder] = i
 
-    for folder in os.listdir(dataset_path):
+    for folder in dataset_dir:
 
         folder_path = os.path.join(dataset_path, folder)
         if not os.path.isdir(folder_path):
@@ -85,20 +94,13 @@ def train_and_test():
     # FACE DETECTION
     faces = []
     labels = []
-    width = 100
 
     for face, label in train_list:
         image = cv.imread(face)
 
-        h, w = image.shape[:2]
-        aspect_ratio = w/h
-        height = int(width/aspect_ratio)
-
         img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        img_gray = cv.resize(img_gray, (width, height))
 
-        img_blur = cv.GaussianBlur(img_gray, (5,5), 0)
-        img = models.detectMultiScale(img_blur)  
+        img = models.detectMultiScale(img_gray, scaleFactor=1.2, minNeighbors=5)  
 
         if(len(img) < 1):
             continue
@@ -108,9 +110,7 @@ def train_and_test():
                 face_img = img_gray[y:y+h, x:x+w]
                 faces.append(face_img)
                 labels.append(label)
-
-    # FACE RECOGNITION
-    recognizer = cv.face.LBPHFaceRecognizer_create()
+                
     recognizer.train(faces, np.array(labels))
 
     correct = 0
@@ -118,16 +118,9 @@ def train_and_test():
 
     for face in test_list:
         image = cv.imread(face)
-
-        h, w = image.shape[:2]
-        aspect_ratio = w/h
-        height = int(width/aspect_ratio)
-
         img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        img_gray = cv.resize(img_gray, (width, height))
 
-        img_blur = cv.GaussianBlur(img_gray, (5,5), 0)
-        img = models.detectMultiScale(img_blur)    
+        img = models.detectMultiScale(img_gray, scaleFactor=1.2, minNeighbors=5)    
 
         if(len(img) < 1):
             continue
@@ -135,7 +128,7 @@ def train_and_test():
             for rect in img:
                 x, y, w, h = rect
                 face_img = img_gray[y:y+h, x:x+w]
-                res, conf = recognizer.predict(face_img)
+                res, _ = recognizer.predict(face_img)
 
                 face_path = os.path.basename(os.path.dirname(face))
                 actual_label = label_map[face_path]
@@ -146,40 +139,49 @@ def train_and_test():
                     correct +=1
 
     avg_accuracy = correct / total_predict * 100
+    model_trained = True
+    
+    # SAVE MODEL
+    recognizer.save('trained_model.yml')
     
     return avg_accuracy
 
 
-
-
 def predict():
-    print('meow')
-    # if models != 0:
-    #     predict_path = input("Input absolute path for image to predict >> ")
-    #     predict_img = cv.imread(predict_path)
-    #     img_gray = cv.imread(predict_img, cv.IMREAD_GRAYSCALE)
+    global model_trained
+    global models
+    global recognizer
+    
+    if not model_trained:
+        print("model not found.")
+        input("press enter to continue...")
+        main()
+    else:
+        print("fetching model...")
+        recognizer.read('trained_model.yml')
         
-    #     face = models.detectMultiScale(img_gray, scaleFactor=1.2, minNeighbors=5)
-        
-    #     for rect in face:
-    #         x, y, w, h = rect
-    #         face_img = img_gray[y:y+h, x:x+w]
-    #         res, conf = recognizer.predict(face_img)
+        predict_path = input("Input absolute path for image to predict >> ")
+        image = cv.imread(predict_path)
+
+        img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        img = models.detectMultiScale(img_gray, scaleFactor=1.2, minNeighbors=5)    
+
+        for rect in img:
+            x, y, w, h = rect
+            face_img = img_gray[y:y+h, x:x+w]
+            res, conf = recognizer.predict(face_img)
             
-    #         conf = math.floor(conf*100)/100
+            conf = math.floor(conf*100)/100
             
-    #         cv.rectangle(predict_img, (x, y), (x+w, y+h), (0, 255, 0), 2)           
-    #         image_text = f"{train_folder[res]} : {str(conf)}%"  
+            # cv.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)           
+            image_text = f"{dataset_dir[res]} : {str(conf)}%"  
+            print(image_text)
             
-    #         cv.putText(predict_img, image_text, (x, y-10), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+            # cv.putText(image, image_text, (x, y-10), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
             
-    #         cv.imshow("Result", predict_img)
-    #         cv.waitKey(0)
-    #         cv.destroyAllWindows()
-    # else:
-    #    print("model not found.")
-    #    input("press enter to continue...")
-    #    main()
+            # cv.imshow("Result", image)
+            # cv.waitKey(0)
+            # cv.destroyAllWindows()
 
 if __name__ == '__main__':
     main()

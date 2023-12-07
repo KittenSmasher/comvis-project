d0f9a700b5f368136d87d7cd94b67b75d32e6be5
KittenSmasher <liaunardicindy@gmail.com>
2023-12-07 19:41:41 +0700

    train and test done

--- project.py
+++ project.py
@@ -4,11 +4,17 @@ import os
 import numpy as np
 import math
 import random
-import shutil
 import time
+
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
 # menu
 def main():
+    clear()
     while True:
         print("Football Player Face Recognition")
         print("1. Train and Test Model")
@@ -17,7 +23,13 @@ def main():
         choice = int(input(">> "))
         
         if choice == 1:
-            train_and_test()
+            print("Training and Testing")
+            time.sleep(0.2)
+            print("calculating accuracy, this may take some time...")
+            avg = train_and_test()
+            print(f"Average Accuracy = {avg:.2f}%")
+            input("Press Enter to Continue...")
+            clear()
         
         elif choice == 2:
             predict()
@@ -31,147 +43,143 @@ def train_and_test():
 
     # datasets
     dataset_path = './Dataset'
+    
+  # split dataset
+    train_list = []
+    test_list = []
 
-    train_path = os.path.join(dataset_path, 'train')
-    test_path = os.path.join(dataset_path, 'test')
-
-    os.makedirs(train_path, exist_ok=True)
-    os.makedirs(test_path, exist_ok=True)
+    images = []
 
     train_ratio = 0.75
 
-    for folder_name in os.listdir(dataset_path):
-        folder_path = os.path.join(dataset_path, folder_name)
-        
+    # convert label to integer
+    label_map = {}
+
+    for i, folder in enumerate(os.listdir(dataset_path)):
+        label_map[folder] = i
+
+    for folder in os.listdir(dataset_path):
+
+        folder_path = os.path.join(dataset_path, folder)
         if not os.path.isdir(folder_path):
             continue
         
-        images = os.listdir(folder_path)
-        
-        # number of train images
-        n = int(len(images) * train_ratio)
-        
-        # random shuffle
+        for img in os.listdir(folder_path):
+            images.append(img)
+
+        num_img = len(images)
+        num_train = int(num_img * train_ratio)
+
         random.shuffle(images)
-        
-        # SPLIT DATA
-        train_img = images[:n]
-        test_img = images[n:]
-        
-        # create separate folder in train folder
-        train_folder = os.path.join(train_path, folder_name)
-        os.makedirs(train_folder, exist_ok=True)
-        
-        # copy train images to train folder
-        for img in train_img:
-            source_path = os.path.join(folder_path, img)
-            dest_path = os.path.join(train_folder, img)
-            shutil.copy(source_path, dest_path)
-            
-        # copy to test images to test folder
-        for img in test_img:
-            source_path = os.path.join(folder_path, img)
-            # rename test images just incase duplicate name
-            dest_path = os.path.join(test_path, f'{folder_name}-{img}')
-            shutil.copy(source_path, dest_path)
-    
-    # delete additional folders that will be made
-    shutil.rmtree('./Dataset/train/train')
-    shutil.rmtree('./Dataset/train/test')
-    
+
+        label = label_map[folder]
+
+        for img in images[:num_train]:
+            img_path = os.path.join(folder_path, img)
+            train_list.append((img_path, label))
+
+        for img in images [num_train:]:
+            img_path = os.path.join(folder_path, img)
+            test_list.append(img_path)
+
     # FACE DETECTION
-    
-    train_path = './Dataset/train/'
-    train_folder = os.listdir(train_path)
-    
     faces = []
     labels = []
-    
-    for idx, folders in enumerate(train_folder):
-        filenames = os.path.join(train_path, folders)
-        
-        for filename in os.listdir(filenames):
-            path = os.path.join(filenames, filename)
-            img_gray = cv.imread(path, cv.IMREAD_GRAYSCALE)
+    width = 100
 
-            face = models.detectMultiScale(img_gray, scaleFactor=1.2, minNeighbors=5)
-            
-            if(len(face) < 1):
-                continue
-            else:
-                for rect in face:
-                    x, y, w, h = rect
-                    face_image = img_gray[y:y+h, x:x+w]
-                    faces.append(face_image)
-                    labels.append(idx)
-    
+    for face, label in train_list:
+        image = cv.imread(face)
+
+        h, w = image.shape[:2]
+        aspect_ratio = w/h
+        height = int(width/aspect_ratio)
+
+        img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        img_gray = cv.resize(img_gray, (width, height))
+
+        img_blur = cv.GaussianBlur(img_gray, (5,5), 0)
+        img = models.detectMultiScale(img_blur)  
+
+        if(len(img) < 1):
+            continue
+        else:
+            for rect in img:
+                x, y, w, h = rect
+                face_img = img_gray[y:y+h, x:x+w]
+                faces.append(face_img)
+                labels.append(label)
+
     # FACE RECOGNITION
     recognizer = cv.face.LBPHFaceRecognizer_create()
     recognizer.train(faces, np.array(labels))
-    
-    # TESTING
-    test_path = './Dataset/test/'
-    
-    accuracy_list = []
-    
-    for filename in os.listdir(test_path):
-        img_path = f"{test_path}/{filename}"
-        test_img = cv.imread(img_path)
-        img_gray = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
-        
-        face = models.detectMultiScale(img_gray, scaleFactor=1.2, minNeighbors=5)
-        
-        if(len(face) < 1):
+
+    correct = 0
+    total_predict = 0
+
+    for face in test_list:
+        image = cv.imread(face)
+
+        h, w = image.shape[:2]
+        aspect_ratio = w/h
+        height = int(width/aspect_ratio)
+
+        img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        img_gray = cv.resize(img_gray, (width, height))
+
+        img_blur = cv.GaussianBlur(img_gray, (5,5), 0)
+        img = models.detectMultiScale(img_blur)    
+
+        if(len(img) < 1):
             continue
         else:
-            for rect in face:
+            for rect in img:
                 x, y, w, h = rect
-                face_image = img_gray[y:y+h, x:x+w]
+                face_img = img_gray[y:y+h, x:x+w]
+                res, conf = recognizer.predict(face_img)
 
-                res, conf = recognizer.predict(face_image)
-                
-                accuracy_list.append(conf)
-        
+                face_path = os.path.basename(os.path.dirname(face))
+                actual_label = label_map[face_path]
+
+                total_predict += 1
+
+                if res == actual_label:
+                    correct +=1
+
+    avg_accuracy = correct / total_predict * 100
     
-    average_accuracy = sum(accuracy_list) / len(accuracy_list)
-    
-    print("Training and Testing")
-    time.sleep(1)
-    print("Training and Testing Finished")
-    time.sleep(0.5)
-    print(f"Average Accuracy = {float(average_accuracy)}")
-    
-    input("press enter to continue...")
-    main()
+    return avg_accuracy
+
+
 
 
 def predict():
-    if models != 0:
-        predict_path = input("Input absolute path for image to predict >> ")
-        predict_img = cv.imread(predict_path)
-        img_gray = cv.imread(predict_img, cv.IMREAD_GRAYSCALE)
+    print('meow')
+    # if models != 0:
+    #     predict_path = input("Input absolute path for image to predict >> ")
+    #     predict_img = cv.imread(predict_path)
+    #     img_gray = cv.imread(predict_img, cv.IMREAD_GRAYSCALE)
         
-        face = models.detectMultiScale(img_gray, scaleFactor=1.2, minNeighbors=5)
+    #     face = models.detectMultiScale(img_gray, scaleFactor=1.2, minNeighbors=5)
         
-        for rect in face:
-            x, y, w, h = rect
-            face_img = img_gray[y:y+h, x:x+w]
-            res, conf = recognizer.predict(face_img)
+    #     for rect in face:
+    #         x, y, w, h = rect
+    #         face_img = img_gray[y:y+h, x:x+w]
+    #         res, conf = recognizer.predict(face_img)
             
-            conf = math.floor(conf*100)/100
+    #         conf = math.floor(conf*100)/100
             
-            cv.rectangle(predict_img, (x, y), (x+w, y+h), (0, 255, 0), 2)           
-            image_text = f"{train_folder[res]} : {str(conf)}%"  
+    #         cv.rectangle(predict_img, (x, y), (x+w, y+h), (0, 255, 0), 2)           
+    #         image_text = f"{train_folder[res]} : {str(conf)}%"  
             
-            cv.putText(predict_img, image_text, (x, y-10), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+    #         cv.putText(predict_img, image_text, (x, y-10), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
             
-            cv.imshow("Result", predict_img)
-            cv.waitKey(0)
-            cv.destroyAllWindows()
-    else:
-       print("model not found.")
-       input("press enter to continue...")
-       main()
+    #         cv.imshow("Result", predict_img)
+    #         cv.waitKey(0)
+    #         cv.destroyAllWindows()
+    # else:
+    #    print("model not found.")
+    #    input("press enter to continue...")
+    #    main()
 
 if __name__ == '__main__':
     main()
